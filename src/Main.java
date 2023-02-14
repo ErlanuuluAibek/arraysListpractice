db368ebff5a864de54e4d564c1ae5ec12be1e957
id251 <danyalider178@gmail.com>
2023-02-14 20:35:55 +0300

    comm

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,13 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         User user=new User(2005,"Aibek",17);
-        User user1=new User(2001,"Meder",22);
+        User user1=new User(2000,"Meder",22);
         User user2=new User(2006,"Malike",16);
-        ArrayList<User>users= new ArrayList<>();
-        users.add(user);
-        users.add(user1);
-        users.add(user2);
         UserDao userDao=new UserDao();
+        userDao.Users(user);
+        userDao.Users(user2);
         userDao.Users(user1);
-        System.out.println(userDao.searchById(2001));
+        System.out.println(userDao.searchById(2000));
 
 
     }
